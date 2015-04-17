57bbc671efe1859b5509af20179ce0b19ab48fdb
Karol Chodakowski <kchodakowski@ptc.com>
2015-04-17 02:53:28 -0500

    Edit conditionally the .bash_profile
    Add sourcing of the .custom_bash_profile to .bash_profile only when it
    doesn't have one.

--- install.sh
+++ install.sh
@@ -6,13 +6,18 @@ cp -v ./custom_bash_profile ~/.custom_bash_profile
 cp -v git-completion.bash ~/.git-completion.bash
 cp -v vimrc ~/.vimrc
 
-echo "Updating ~/.bash_profile ..."
-cat <<LOAD_CUSTOM_PROFILE >> ~/.bash_profile
+
+if grep -q "If custom profile exists" ~/.bash_profile; then
+    echo "File ~/.bash_profile is aware of custom_bash_profile"
+else
+    echo "Updating ~/.bash_profile ..."
+    cat <<LOAD_CUSTOM_PROFILE >> ~/.bash_profile
 # If custom profile exists, load it
 if [ -f ~/.custom_bash_profile ]; then
    echo "Loading your custom profile from ~/.custom_bash_profile"
    . ~/.custom_bash_profile
 fi
 LOAD_CUSTOM_PROFILE
+fi
 
 echo "Done."
